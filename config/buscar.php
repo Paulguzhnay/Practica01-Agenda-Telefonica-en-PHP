723d68131d0a7e817e07ba5b286a31147991dfaf
iamjosephreinoso <69018206+iamjosephreinoso@users.noreply.github.com>
2021-05-23 17:06:45 -0500

    Buscar por correo

--- config/buscar.php
+++ config/buscar.php
@@ -4,7 +4,7 @@
  $cedula = $_GET['cedula'];
  //echo "Hola " . $cedula;
 
- if(strlen($cedula)==10){
+if(strlen($cedula)==10){
     $sql = "SELECT * FROM usuarios WHERE usu_eliminado = 'N' and usu_cedula='$cedula'";
     $sql2 ="SELECT * FROM telefonos WHERE usuarios_usu_id ='$cedula'";
     $result = $conn->query($sql);
@@ -26,12 +26,7 @@
     <th>Tipo</th>
     <th>Operadora</th>
     </tr>";
-
     //-----------------
-
-
-
-
     if ($result->num_rows > 0) {
     while($row = $result->fetch_assoc()) {
 
@@ -59,58 +54,58 @@
     echo "</table>";
     $conn->close();
 }else {
-    $sql = "SELECT * FROM usuarios WHERE usu_eliminado = 'N' and usu_mail='$cedula'";
-    $result = $conn->query($sql);
-    //cambiar la consulta para puede buscar por ocurrencias de letras
-    //$result2= $conn->query($sql2);
-    echo " <table style='width:100%' border='1' align='center'>
-    <tr>
-    <th colspan='5'>  Datos Personales </th>
-    <th colspan ='3'>  Teléfonos</th>
-    </tr>
-    <tr>
-    <th>Cédula</th>
-    <th>Nombres</th>
-    <th>Apellidos</th>
-    <th>Correo</th>
-    <th>Fecha Nacimiento</th>
-    <th>Teléfono</th>
-    <th>Tipo</th>
-    <th>Operadora</th>
-    </tr>";
-
-    //-----------------
-
-
-
-
-    if ($result->num_rows > 0) {
-    while($row = $result->fetch_assoc()) {
-
-    echo "<tr>";
-    echo " <td>" . $row['usu_cedula'] . "</td>";
-    echo " <td>" . $row['usu_nombre'] ."</td>";
-    echo " <td>" . $row['usu_apellido'] . "</td>";
-    echo " <td>" . $row['usu_mail'] . "</td>";
-    echo " <td>" . $row['usu_nacimiento'] . "</td>";
-    
-        //if ($result2->num_rows > 0){
-        //    while ($row = $result2->fetch_assoc()){
-         //       echo " <td>" . $row['telf_numero'] . "</td>";
-           //     echo " <td>" . $row['telf_operadora'] ."</td>";
-             //   echo " <td>" . $row['telf_tipo'] . "</td>";
-              //  echo "</tr>";
-            //}
-        //}
-    }
-    } else {
-    echo "<tr>";
-    echo " <td colspan='7'> No existen usuarios registradas en el sistema </td>";
-    echo "</tr>";
-    }
-    echo "</table>";
-    $conn->close();
-    }
-
-
+        $sql = "SELECT usu_cedula FROM usuarios WHERE usu_eliminado = 'N' and usu_mail='$cedula'";
+        $result = $conn->query($sql);
+        while ($row1 = $result->fetch_assoc()){
+            if($row1['usu_cedula']){
+                $cedulacom = $row1['usu_cedula'];
+            }
+        }
+        echo($cedulacom);
+        $sql2 ="SELECT * FROM telefonos WHERE usuarios_usu_id ='$cedulacom'";
+        $result2= $conn->query($sql2);
+        //cambiar la consulta para puede buscar por ocurrencias de letras
+        //$result2= $conn->query($sql2);
+        echo " <table style='width:100%' border='1' align='center'>
+        <tr>
+        <th colspan='5'>  Datos Personales </th>
+        <th colspan ='3'>  Teléfonos</th>
+        </tr>
+        <tr>
+        <th>Cédula</th>
+        <th>Nombres</th>
+        <th>Apellidos</th>
+        <th>Correo</th>
+        <th>Fecha Nacimiento</th>
+        <th>Teléfono</th>
+        <th>Tipo</th>
+        <th>Operadora</th>
+        </tr>";
+        //-----------------
+        if ($result->num_rows > 0) {
+        while($row = $result->fetch_assoc()) {
+        echo "<tr>";
+        echo " <td>" . $row['usu_cedula'] . "</td>";
+        echo " <td>" . $row['usu_nombre'] ."</td>";
+        echo " <td>" . $row['usu_apellido'] . "</td>";
+        echo " <td>" . $row['usu_mail'] . "</td>";
+        echo " <td>" . $row['usu_nacimiento'] . "</td>";
+        
+            if ($result2->num_rows > 0){
+                while ($row = $result2->fetch_assoc()){
+                echo " <td>" . $row['telf_numero'] . "</td>";
+                    echo " <td>" . $row['telf_operadora'] ."</td>";
+                    echo " <td>" . $row['telf_tipo'] . "</td>";
+                    echo "</tr>";
+                }
+            }
+        }
+        } else {
+        echo "<tr>";
+        echo " <td colspan='7'> No existen usuarios registradas en el sistema </td>";
+        echo "</tr>";
+        }
+        echo "</table>";
+        $conn->close();
+        }
 ?>
