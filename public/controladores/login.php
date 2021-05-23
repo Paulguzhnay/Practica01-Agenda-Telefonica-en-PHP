ae624816504c0f79ead2a9d119de0a0318b1d878
iamjosephreinoso <69018206+iamjosephreinoso@users.noreply.github.com>
2021-05-23 17:43:14 -0500

    Login detectar si Admin o User

--- public/controladores/login.php
+++ public/controladores/login.php
@@ -3,9 +3,18 @@
     include '../../config/conexionBD.php';
     $usuario = isset($_POST["correo"]) ? trim($_POST["correo"]) : null;
     $contrasena = isset($_POST["contrasena"]) ? trim($_POST["contrasena"]) : null;
-    $sql = "SELECT * FROM usuarios WHERE usu_mail = '$usuario' and usu_password = MD5('$contrasena')";
+    $sql = "SELECT * FROM usuarios WHERE usu_mail = '$usuario' and usu_password = MD5('$contrasena') and usu_rol = 'Administrador'";
     $result = $conn->query($sql);
     if ($result->num_rows > 0) {
+        $_SESSION['isLogged'] = TRUE;
+        header("Location: indexBusqueda.php");
+    } else {
+        header("Location: ../vista/login.html");
+    }
+
+    $sql2 = "SELECT * FROM usuarios WHERE usu_mail = '$usuario' and usu_password = MD5('$contrasena') and usu_rol = 'Usuario'";
+    $result2 = $conn->query($sql2);
+    if ($result2->num_rows > 0) {
         $_SESSION['isLogged'] = TRUE;
         header("Location: indexBusqueda.php");
     } else {
