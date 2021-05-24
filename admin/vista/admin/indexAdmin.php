22d232780f1b34cb0cfe3aa24b81ea3798929578
Paulguzhnay <69018647+Paulguzhnay@users.noreply.github.com>
2021-05-23 21:12:55 -0500

    creacion navegacion

---
+++ admin/vista/admin/indexAdmin.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link href="../../../css/estilo.css" rel="stylesheet" />
+    <link href="../../../css/layout.css" rel="stylesheet" />
+    <meta charset="UTF-8">
+    <script type="text/javascript" src="../../controladores/user/buscarUser.js"></script>
+    <title>Gestión de Usuarios</title>
+</head>
+<body>
+    <header>
+        <a href="../../../../index.html"><img src="../../../images/Agenda Telefonica.jpg"></a>
+    </header>
+<!--
+    BOTON CEDULA 
+-->
+
+    <br>
+
+<!--
+    TABLA DE OPCIONES 
+-->
+<table style='width:100%' border='1' align='center'>
+<table style="width:100%">
+ <tr>
+
+    <th> <a href="../../../public/vista/crearUsuario.html">Agregar</a></th>
+    <th> <a href="../../controladores/admin/modificar.php">Modificar </a></th>
+    <th> <a href="../../controladores/admin/buscarAdmin.php">Buscar</a></th>
+    <th> <a href="../../controladores/admin/eliminar.php"> Eliminar</a></th>
+    <th> <a href="../../controladores/admin/listar.php" >Listar</a></th>
+    <th><a href="../../controladores/admin/cambiarContra.php">Cambiar Contraseña</a></th>
+ </tr>
+ <table style='width:100%' border='1' align='center'>
+ </table>
+
+
+<br>
+<br>
+<br>
+
+
+</body>
+<footer>
+      Paul Guzhñay &amp; Joseph Reinoso - Universidad Politécnica Salesiana 
+        <br/>&copy; Todos los derechos reservados
+</footer>
+</html>
---
+++ admin/vista/user/indexUsuario.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link href="../../../css/estilo.css" rel="stylesheet" />
+    <link href="../../../css/layout.css" rel="stylesheet" />
+    <meta charset="UTF-8">
+    <script type="text/javascript" src="../../controladores/user/buscarUser.js"></script>
+    <title>Gestión de Usuarios</title>
+</head>
+<body>
+    <header>
+        <a href="../../../../index.html"><img src="../../../images/Agenda Telefonica.jpg"></a>
+    </header>
+<!--
+    BOTON CEDULA 
+-->
+
+    <br>
+
+<!--
+    TABLA DE OPCIONES 
+-->
+<table style='width:100%' border='1' align='center'>
+<table style="width:100%">
+ <tr>
+
+    <th> <a href="../../controladores/user/modificar.php">Modificar </a></th>
+    <th> <a href="../../controladores/user/eliminar.php"> Eliminar</a></th>
+    <th><a href="../../controladores/user/cambiarContra.php">Cambiar Contraseña</a></th>
+ </tr>
+ <table style='width:100%' border='1' align='center'>
+ </table>
+
+
+<br>
+<br>
+<br>
+
+
+</body>
+<footer>
+      Paul Guzhñay &amp; Joseph Reinoso - Universidad Politécnica Salesiana 
+        <br/>&copy; Todos los derechos reservados
+</footer>
+</html>
